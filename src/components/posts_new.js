03e9b03356179c5e499916880f23ba6a01f8278c
Brent O'Neill <brent.oneill@trackvia.com>
2017-02-15 16:37:30 -0700

    Added form validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -12,17 +12,26 @@ class PostsNew extends Component {
         return (
             <form onSubmit={handleSubmit(this.props.createPost)}>
                 <h3>Create a New Post</h3>
-                <div className="form-group">
+                <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
                     <label>Title</label>
                     <input type="text" className="form-control" {...title} />
+                    <div className="text-help">
+                        {title.touched ? title.error: ''}
+                    </div>
                 </div>
-                <div className="form-group">
+                <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
                     <label>Categories</label>
                     <input type="text" className="form-control" {...categories} />
+                    <div className="text-help">
+                        {categories.touched ? categories.error: ''}
+                    </div>
                 </div>
-                <div className="form-group">
+                <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
                     <label>Content</label>
                     <textarea className="form-control" {...content} />
+                    <div className="text-help">
+                        {content.touched ? content.error: ''}
+                    </div>
                 </div>
                 <button type="submit"
                         className="btn btn-primary">
@@ -33,6 +42,26 @@ class PostsNew extends Component {
     }
 }
 
+function validate(values) {
+    const errors = {};
+
+    if (!values.title) {
+        // NOTE: errors[key], key must match the field name
+        errors.title = 'Enter a title';
+    }
+
+    if (!values.categories) {
+        // NOTE: errors[key], key must match the field name
+        errors.categories = 'Enter some categories';
+    }
+
+    if(!values.content) {
+        errors.content = 'Enter some content';
+    }
+
+    return errors;
+}
+
 // connect: first arg is mapStateToProps, 2nd is mapDispatchToProps
 // reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
 
@@ -41,7 +70,8 @@ export default reduxForm({
     form: 'PostsNewForm',
     // Tell redux-form to watch these inputs
     // Used above to inject in to correspoinding inputs
-    fields: ['title', 'categories', 'content']
+    fields: ['title', 'categories', 'content'],
+    validate
 }, null, { createPost })(PostsNew);
 
 // user types something in ... record it on the application state
