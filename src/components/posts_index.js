8c285a561577d8cbae877986e8fedbee31f0ec8e
Brent O'Neill <brent.oneill@trackvia.com>
2017-02-17 09:38:08 -0700

    Working on posts show

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -11,25 +11,42 @@ class PostsIndex extends Component {
         this.props.fetchPosts();
     }
 
+    renderPosts() {
+        return this.props.posts.map(post => {
+            return (
+                <li className="list-group-item" key={post.id}>
+                    <span className="pull-xs-right">{post.categories}</span>
+                    <strong>{post.title}</strong>
+                </li>
+            )
+        });
+    }
+
     render() {
         return (
             <div className="clearfix">
-                <div className="inline-block lead pull-left">
-                    List of blog posts
-                </div>
                 <div className="pull-right">
                     <Link to="/posts/new" className="btn btn-primary">
                         Add a post
                     </Link>
                 </div>
-
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
             </div>
         );
     }
 }
 
+function mapStateToProps(state) {
+    return {
+        posts: state.posts.all
+    };
+}
+
 // 1st arg is mapStateToProps, 2nd is mapDispatchToProps
-export default connect(null, {
+export default connect(mapStateToProps, {
     fetchPosts
 })(PostsIndex);
 
